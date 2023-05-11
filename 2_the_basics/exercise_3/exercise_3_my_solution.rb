85d3d9c620c2074308b6a5d171e83ed5f936f2f8
MarilynRoxie <marilynroxie@gmail.com>
2023-05-11 10:12:48 +0100

    Added rest of solutions for The Basics section

---
+++ 2_the_basics/exercise_3/exercise_3_my_solution.rb
@@ -0,0 +1,7 @@
+movies = {
+  Blue: 1993,
+  The_Doom_Generation: 1995,
+  Velvet_Goldmine: 1998,
+}
+
+movies.each { |movie, year| puts year }
---
+++ 2_the_basics/exercise_4/exercise_4_my_solution.rb
@@ -0,0 +1,5 @@
+dates = [1993, 1995, 1998]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
