c69fc24bfd162e726f8243d8eee3d42992afabe2
MarilynRoxie <marilynroxie@gmail.com>
2023-05-13 16:50:04 +0100

    Add solutions for Methods

---
+++ 4_methods/exercise_6/exercise_6_my_solution.rb
@@ -0,0 +1,10 @@
+=begin
+ArgumentError: wrong number of arguments (1 for 2)
+  from (irb):1:in `calculate_product'
+  from (irb):4
+  from /Users/username/.rvm/rubies/ruby-2.5.3/bin/irb:12:in 
+`<main>'
+
+You have only indicated one argument when two are required.
+
+=end
